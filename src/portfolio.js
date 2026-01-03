d5ed5e43e05b3189c296187e3f2dd4b543935f97
Eden Takele Abtew <se.eden.takele@gmail.com>
2026-01-03 15:59:59 +0100

    second commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,8 +20,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Eden Takele",
-  title: "Hi all, I'm Eden Takele",
+  username: "Eden Takele Abtew",
+  title: "Hi all, I'm Eden Takele Abtew",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
